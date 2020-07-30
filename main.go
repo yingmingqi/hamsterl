3e64913c726e8b6aba5c555dea6ed3bb890d1317
yingmingqi <ym_qi@outlook.com>
2020-07-30 11:03:02 +0800

    init

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"hamsterl/controller"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", handlers.Home).Methods("GET")
+	router := controller.Router()
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:80",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println(time.Now().Format("2006-01-02T15:04:05Z07:00"), " - Services Start")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalln("ListenAndServe err:", err)
+	}
 }
