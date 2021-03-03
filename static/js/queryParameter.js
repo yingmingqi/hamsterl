03401b9ffd6b21e9f27cac9b6b3d1bac7beb55b3
yingmingqi <ym_qi@outlook.com>
2021-03-03 18:00:16 +0800

    change

--- static/js/queryParameter.js
+++ static/js/queryParameter.js
@@ -1,64 +1,73 @@
 /* 
     查询参数对象
 */
-class QueryArgs {
+
+class QueryArgsMap {
     constructor() {
         this.Parameter = {
             And: {},
-            Or: {},
-            Sort: {}
+            Or: {}
         }
-        this.adAndParam = function (flag) {
+        this.SetAnd = function (flag) {
             let k = flag.split(':')[0],
                 v = flag.split(':')[1];
             this.Parameter.And[k] = v;
         }
-        this.adOrParam = function (flag) {
+        this.SetOr = function (flag) {
             let k = flag.split(':')[0],
                 v = flag.split(':')[1];
             if (k in this.Parameter.Or) {
                 if (this.Parameter.Or[k].indexOf(v) > -1) {
                     console.log(v)
-                    this.Parameter.Or[k].splice(this.Parameter.Or[k].indexOf(v),1)
-                }
-                else {
+                    this.Parameter.Or[k].splice(this.Parameter.Or[k].indexOf(v), 1)
+                } else {
                     this.Parameter.Or[k].push(v)
                 }
             } else {
                 this.Parameter.Or[k] = [v]
             }
         }
-        this.delOrParam = function (flag) {
-            this.adOrParam(flag);
+        this.ContainsValue = function (mod, flag) {
+            if ("and" === mod) {
+                let k = flag.split(':')[0],
+                    v = flag.split(':')[1];
+                if (!(k in this.Parameter.And)) {
+                    return false
+                }
+                if (!(v === this.Parameter.And[k])) {
+                    return false
+                }
+                return true
+            }
+            if ("or" === mod) {
+                let k = flag.split(':')[0],
+                    v = flag.split(':')[1];
+                if (!(k in this.Parameter.Or)) {
+                    return false
+                }
+                if (-1 === this.Parameter.Or[k].indexOf(v)) {
+                    return false
+                }
+                return true
+            }
         }
-        this.changeSort = function (flag) {
-            let k = flag.split(':')[0],
-                v = flag.split(':')[1],
-                tmp = {}
-            tmp[k] = v;
-            this.Parameter.Sort = tmp
-        }
-    };
-    newObj(pm) {
+
+    }
+
+    newMap(pm) {
         this.Parameter = pm;
         return this;
     }
     pop(obj) {
         switch (obj.dataset.mod) {
             case 'and':
-                this.adAndParam(obj.dataset.flag)
+                this.SetAnd(obj.dataset.flag)
                 break
             case 'or':
-                this.adOrParam(obj.dataset.flag)
+                this.SetOr(obj.dataset.flag)
                 break
-            case 'sort':
-                this.changeSort(obj.dataset.flag)
-                break
-            case 'del':
-                this.delOrParam(obj.dataset.flag)
             default:
                 break
         }
     }
-
 }
--- static/js/templateTextHomeJS.js
+++ static/js/templateTextHomeJS.js
@@ -1,14 +1,33 @@
 /* 
     单向绑定 > tag选项
 */
-const QA = new QueryArgs().newObj({ And: {"serialstatus":"all"},Or: {},Sort: {"sort":"updatetimedesc"} })
+const QA = new QueryArgsMap().newMap({ And: {"serialstatus":"all"},Or: {},Sort: {"sort":"updatetimedesc"} })
 const Hanlder = function(){}
 const ProxyHanlder = {
     apply: function(target, thisArg, argumentsList){
         let origin = JSON.parse(JSON.stringify(QA.Parameter)) // 深拷贝
-        console.log("arg list : ${argumentsList}")
-        console.log(argumentsList[0].dataset.mod,argumentsList[0].dataset.flag)
-        QA.pop(argumentsList[0])
+        console.log(`argumentsList is: ${argumentsList}`)
+        console.log(`thisArg is: ${thisArg}`)
+        console.log(`target is: ${target}`)
+        let thisobj = argumentsList[0]
+        console.log(`mod is: ${thisobj.dataset.mod} , flag is: ${thisobj.dataset.flag}`)
+        console.log(`fathernode is: ${thisobj.parentNode}`)
+        for (let node of thisobj.parentNode.children) {node.classList.remove('current')}
+        thisobj.classList.add('current')
+        QA.pop(thisobj)
+        return
+    }
+}
+const ProxyHanlder2 = {
+    apply: function(target, thisArg, argumentsList){
+        let origin = JSON.parse(JSON.stringify(QA.Parameter)) // 深拷贝
+        console.log(`argumentsList is: ${argumentsList}`)
+        console.log(`thisArg is: ${thisArg}`)
+        console.log(`target is: ${target}`)
+        let thisobj = argumentsList[0]
+        console.log(`mod is: ${thisobj.dataset.mod} , flag is: ${thisobj.dataset.flag}`)
+        if (QA.ContainsValue(thisobj.dataset.mod,thisobj.dataset.flag)){ thisobj.classList.remove('current') } // 改变标签状态
+        QA.pop(thisobj)
         return
     }
 }
