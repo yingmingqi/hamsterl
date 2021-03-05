b5965653a872cd53b9c7ffd53fd5d2fdecbf572f
yingmingqi <ym_qi@outlook.com>
2021-03-05 17:59:02 +0800

    111

--- static/js/templateTextHomeJS.js
+++ static/js/templateTextHomeJS.js
@@ -3,6 +3,7 @@
 */
 const QA = new QueryArgsMap().newMap({ And: {"serialstatus":"all"},Or: {},Sort: {"sort":"updatetimedesc"} })
 const Hanlder = function(){}
+const Hanlder2 = function(){}
 const ProxyHanlder = {
     apply: function(target, thisArg, argumentsList){
         let origin = JSON.parse(JSON.stringify(QA.Parameter)) // 深拷贝
@@ -26,9 +27,17 @@ const ProxyHanlder2 = {
         console.log(`target is: ${target}`)
         let thisobj = argumentsList[0]
         console.log(`mod is: ${thisobj.dataset.mod} , flag is: ${thisobj.dataset.flag}`)
-        if (QA.ContainsValue(thisobj.dataset.mod,thisobj.dataset.flag)){ thisobj.classList.remove('current') } // 改变标签状态
+        if (!QA.ContainsValue(thisobj.dataset.mod,thisobj.dataset.flag)){
+            thisobj.classList.add('current') // 添加类标签
+            let node = new el('span',{class: 'delspan'},[new el('svg',{viewBox:'0 0 24 24',class:'icon-del','aria-hidden':'true'},[new el('path',{d:'M6 6L17 17M6 17L17 6'},[])])]).render() // seleded 添加 node
+            let clnode = thisobj.cloneNode(true)
+            clnode.appendChild(node)
+            clnode.innerHTML = clnode.innerHTML // 解决 css 不显示
+            document.getElementById('selected').appendChild(clnode)
+        }
         QA.pop(thisobj)
         return
     }
 }
 const SelectedTag = new Proxy(Hanlder,ProxyHanlder)
+const SelectedTag2 = new Proxy(Hanlder2,ProxyHanlder2)
